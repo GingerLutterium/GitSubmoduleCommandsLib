ae573aa51f48d0f443fdf52e3b7855e6ac6857b9
GingerLutterium <117631451+GingerLutterium@users.noreply.github.com>
2022-11-10 21:37:31 +0200

    Add files via upload

---
+++ GitSubmoduleCommandsLib/SubmoduleCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitSubmoduleCommandsLib
+{
+    public class SubmoduleCommands
+    {
+        List<string> list = new List<string>();
+        public void SubmoduleCommandGit(string command_git)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "cmd.exe";
+            info.Verb = "runas";
+            info.Arguments = $"git submodule {command_git}";
+            info.WindowStyle = ProcessWindowStyle.Normal;
+            info.UseShellExecute = true;
+            info.CreateNoWindow = false;
+            Process.Start(info);
+        }
+    }
+}
